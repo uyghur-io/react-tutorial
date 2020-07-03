6809822c904161897b119cf2f385a0e669f5f944
uyghur-io <admin@uyghur.io>
2020-07-03 19:19:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Header.js';
 import Nav from './Nav.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
