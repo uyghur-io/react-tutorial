58793958aea0916036258bf7e37da6e16be88998
uyghur-io <admin@uyghur.io>
2020-07-04 12:35:54 +0300

    -

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Nav />
       <Main />
-     <Footer />
+      <Footer />
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,11 @@
 import React from  'react'
-import { Link } from 'react-router'
 
-class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+function Main() {
+    return(
+        <div>
+            <p>this is main</p>
+        </div>
+    )
 }
 
 export default Main
